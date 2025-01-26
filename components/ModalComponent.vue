87f3d7f8e2682134e44d3396a3f3743c1367f5ee
kawa054 <kawa054ss@gmail.com>
2025-01-26 11:06:21 +0900

    add modal system

--- components/ModalComponent.vue
+++ components/ModalComponent.vue
@@ -1,31 +1,92 @@
+<script setup lang="ts">
+    interface Props {
+        imageName?: string|null;
+        description?: string|null;
+        modalTitle?: string|null;
+    }
+    const props = withDefaults(defineProps<Props>(), {
+        imageName: null,
+        description: null,
+        modalTitle: null,
+    });
+
+    //imgのpathを生成する関数
+    const generateImgPath = (fileName: string): string => {
+        return new URL(`../assets/logo/${fileName}.png`, import.meta.url).href
+    }
+
+</script>
+
 <template>
-  <div class="modal">
-    <div class="modal-content">
-      <span class="close" @click="$emit('close')">&times;</span>
-      <p>Some text in the Modal..</p>
-    </div>
-  </div>
+    <!-- teleportでネストされた DOM 構造から抜け出せるようにする -->
+    <Teleport to="html, body">
+        <div class="modal">
+            <div class="modal-content">
+                <span class="close" @click="$emit('close')">&times;</span>
+                <div class="row-content">
+                    <img :src="generateImgPath(imageName)">
+                    <div class="modal-title">{{ modalTitle }}</div>
+                    <div class="modal-separator"></div>
+                </div>
+                <div class="modal-description">{{ description }}</div>
+                <div class="css-kanzenni-rikai" v-if="modalTitle === 'CSS'">CSS完全に理解したい</div>
+            </div>
+        </div>
+    </Teleport>
 </template>
 
-<style>
+<style lang="scss" scoped>
     .modal {
         position: fixed; 
-        z-index: 3; 
+        z-index: 999; 
         left: 0;
         top: 0;
         width: 100%; 
         height: 100%; 
-        overflow: auto; 
+        overflow: hidden;
         background-color: rgb(0,0,0); 
-        background-color: rgba(0,0,0,0.4); 
+        background-color: rgba(0,0,0,0.6); 
     }
 
     .modal-content {
-        background-color: #fefefe;
-        margin: 15% auto;
-        padding: 20px;
+        background-color: #fff;
+        margin: 10% auto;
         border: 1px solid #888;
-        width: 80%; 
+        width: 30%; 
+        height: 60%;
+		border-radius: 10px;
+		border: 4px solid #000;
+        .row-content {
+            margin-top: 8%;
+            display:flex;
+            flex-flow: column;
+            align-items: center;
+            .modal-title{
+                padding-top: 2%;
+                padding-bottom: 2%;
+                font-size: 23px;
+            }
+            .modal-separator{
+                width: 80%;
+                border-bottom: 4px solid #42a30a4d;
+            }
+        }
+        .modal-description{
+            margin: 12%;
+            margin-top: 3%;
+            margin-left: 14%;
+            font-size: 20px;
+        }
+        .css-kanzenni-rikai{
+            position: absolute;
+            padding-left: 25%;
+            color: rgb(255, 0, 0);
+        }
+    }
+
+    img {
+        width: 100px;
+        height: 100px;
     }
 
     .close {
@@ -33,6 +94,8 @@
         float: right;
         font-size: 28px;
         font-weight: bold;
+        padding-right: 10px;
+        padding-top: 3px;
     }
 
     .close:hover,
--- components/about/SkillsViewComponent.vue
+++ components/about/SkillsViewComponent.vue
@@ -4,7 +4,11 @@
     
     import ModalComponent from "~/components/ModalComponent.vue";
 
-    const is_modal_opened = ref(true);
+    //モーダル用データ
+    const is_modal_opened = ref(false);
+    const imageName = ref("c");
+    const title = ref("undefined title");
+    const description = ref("undefined description");
 
 	gsap.registerPlugin(ScrollTrigger);
 
@@ -15,49 +19,171 @@
 </script>
 
 <template>
+    <ModalComponent v-if="is_modal_opened" @close="is_modal_opened = false" :imageName=imageName :modalTitle=title :description=description />
     <div class="view-container">
         <div class="skill-row">
-            <img class="star" src="/assets/logo/star4.png" @click="is_modal_opened = true">
+            <img class="star" src="/assets/logo/star4.png" @click="
+                is_modal_opened = true;
+                imageName = 'star4';
+                title = '技術レベル4';
+                description = '問題なく言語を使えるだけでなく、その言語のライブラリを作ったり、フレームワークを作ることもできる';
+            ">
             <div class="split-line"></div>
             <ul class="skill">
-                <li><img src="/assets/logo/c.png"></li>
-                <li><img src="/assets/logo/java.png"></li>
+                <li><img src="/assets/logo/c.png" @click="
+                    is_modal_opened = true;
+                    imageName = 'c';
+                    title = 'C言語';
+                    description = '高専での授業、大学での授業での成果物に使用。５年以上触れており、問題なく様々なものを作成できる。';
+                ">
+                </li>
+                <li><img src="/assets/logo/java.png" @click="
+                    is_modal_opened = true;
+                    imageName = 'java';
+                    title = 'Java言語';
+                    description = '高専での授業や普段の開発等で使用し、オブジェクト指向型言語の中で最も理解している言語。ゲームなどのやや規模の大きい開発なども行ったことがあり、独立性、再利用性、拡張性を考えつつ開発を行える。UMLを用い、システムの振る舞いを記述する事も可能。JavaベースのProcessingでの開発も得意。';
+                "></li>
             </ul>
         </div>
         <div class="skill-row">
-            <img class="star" src="/assets/logo/star3.png">
+            <img class="star" src="/assets/logo/star3.png" @click="
+                is_modal_opened = true;
+                imageName = 'star3';
+                title = '技術レベル3';
+                description = '言語文法･開発環境について理解しており、必要に応じてリファレンスを見ながらプログラムを開発できる';
+            ">
             <div class="split-line"></div>
             <ul class="skill">
-                <li><img src="/assets/logo/cpp.png"></li>
-                <li><img src="/assets/logo/c-sharp.png"></li>
-                <li><img src="/assets/logo/git.png"></li>
-                <li><img src="/assets/logo/python.png"></li>
-                <li><img src="/assets/logo/unity.png"></li>
-                <li><img src="/assets/logo/html.png"></li>
-                <li><img src="/assets/logo/vue.png"></li>
-                <li><img src="/assets/logo/gml.png"></li>
+                <li><img src="/assets/logo/cpp.png" @click="
+                    is_modal_opened = true;
+                    imageName = 'cpp';
+                    title = 'C++';
+                    description = '主に競技プログラミングで使用しており、Java言語と同じくリファレンスを参照しつつある程度の規模の開発を行うことが出来る。また、高専時の一部の数値計算プログラムなども作成していた。';
+                "></li>
+                <li><img src="/assets/logo/c-sharp.png" @click="
+                    is_modal_opened = true;
+                    imageName = 'c-sharp';
+                    title = 'C#';
+                    description = 'Unityでのプログラムや、高専でのWindows GUIアプリケーション開発等で主に使用。リファレンスなどを見つつ中規模程度の開発ならば可能。';
+                "></li>
+                <li><img src="/assets/logo/git.png" @click="
+                    is_modal_opened = true;
+                    imageName = 'git';
+                    title = 'git関係';
+                    description = '高専プログラミングコンテストやインターンシップ、ロボコン等でのチーム開発で主に使用。Gitlabを使い、issue作成をしつつ振りかえりを行うようなチーム開発も経験。また、一部の個人開発でも使用しており、基礎的なコマンドは理解している。';
+                "></li>
+                <li><img src="/assets/logo/python.png" @click="
+                    is_modal_opened = true;
+                    imageName = 'python';
+                    title = 'Python';
+                    description = '高専、大学の授業や研究等で主に使用しており、数値計算やシミュレーション、グラフのプロット等を行える。また、TensorFlowやPytorchなどの深層学習関連のコードや、ロボットの画像認識プログラム等の作成経験もある。必要に応じて、Javaのようにオブジェクト指向を意識したクラスの作成等も可能で、中規模程度のアプリケーションであれば作成可能。';
+                "></li>
+                <li><img src="/assets/logo/unity.png" @click="
+                    is_modal_opened = true;
+                    imageName = 'unity';
+                    title = 'Unity';
+                    description = '主に個人での3Dゲーム開発で使用。学祭でゲームランチャー等のUIアプリケーションも作成したことがあり、中規模のゲームを作成可能。';
+                "></li>
+                <li><img src="/assets/logo/html.png" @click="
+                    is_modal_opened = true;
+                    imageName = 'html';
+                    title = 'HTML';
+                    description = '学校での授業や、WEB系開発アルバイト、このポートフォリオ制作等で使用。基礎的な知識はあり、特に問題なく理解できる。';
+                "></li>
+                <li><img src="/assets/logo/vue.png" @click="
+                    is_modal_opened = true;
+                    imageName = 'vue';
+                    title = 'Vue3';
+                    description = 'WEB系開発アルバイトと、このポートフォリオサイトの作成で使用。適当なサイトやリファレンスを参照しながら中規模程度のWEBアプリの開発も可能。外部のライブラリや新しい要素の追加にも対応でき、WEB系の開発アルバイトではVue2からVue3への移行作業なども行った経験がある。';
+                "></li>
+                <li><img src="/assets/logo/gml.png" @click="
+                    is_modal_opened = true;
+                    imageName = 'gml';
+                    title = 'GameMaker Studio';
+                    description = '主に個人での2Dゲーム開発で使用。大きめのゲームを作成したこともあり、Unityと同じくらい扱うことができる。内部の独自言語であるGMLも特に問題なく使いこなすことができ、2Dゲームならば大きめのゲームも作成可能。';
+                "></li>
             </ul>
         </div>
         <div class="skill-row">
-            <img class="star" src="/assets/logo/star2.png">
+            <img class="star" src="/assets/logo/star2.png" @click="
+                is_modal_opened = true;
+                imageName = 'star2';
+                title = '技術レベル2';
+                description = '言語文法･開発環境のリファレンスを常に見ながらであればプログラムを開発できる';
+            ">
             <div class="split-line"></div>
             <ul class="skill">
-                <li><img src="/assets/logo/opengl.png"></li>
-                <li><img src="/assets/logo/linux.png"></li>
-                <li><img src="/assets/logo/react.png"></li>
-                <li><img src="/assets/logo/css.png"></li>
-                <li><img src="/assets/logo/js.png"></li>
-                <li><img src="/assets/logo/ts.png"></li>
-                <li><img src="/assets/logo/sql.png"></li>
-                <li><img src="/assets/logo/php.png"></li>
+                <li><img src="/assets/logo/opengl.png" @click="
+                    is_modal_opened = true;
+                    imageName = 'opengl';
+                    title = 'OpenGL';
+                    description = '2Dゲームなどでのシェーダーの開発時に使用。GLSLでのシェーダー作成を行うことができ、ある程度基礎的な知識は身に付けている。';
+                "></li>
+                <li><img src="/assets/logo/linux.png" @click="
+                    is_modal_opened = true;
+                    imageName = 'linux';
+                    title = 'Linux';
+                    description = '基礎的なコマンドはもちろん、WSLを用いた開発もコンテスト等で行ったことがあり問題なく使用できる。Raspberry Piを用いた電子工作も一部可能。';
+                "></li>
+                <li><img src="/assets/logo/react.png" @click="
+                    is_modal_opened = true;
+                    imageName = 'react';
+                    title = 'React Native';
+                    description = '高専プログラミングコンテストの成果物に使用。iOSとAndroid上で動作する簡単なアプリケーションや、やや中規模程度の開発を行うことが出来る。';
+                "></li>
+                <li><img src="/assets/logo/css.png" @click="
+                    is_modal_opened = true;
+                    imageName = 'css';
+                    title = 'CSS';
+                    description = '学校の授業やWEB系開発アルバイト、このポートフォリオサイト等で使用。調べながら、HTMLと併せて特に問題なく扱える。';
+                "></li>
+                <li><img src="/assets/logo/js.png" @click="
+                    is_modal_opened = true;
+                    imageName = 'js';
+                    title = 'JavaScript';
+                    description = '学校の授業やWEB系開発アルバイト、このポートフォリオサイト等で使用。調べながら、HTMLと併せて特に問題なく扱える。';
+                "></li>
+                <li><img src="/assets/logo/ts.png" @click="
+                    is_modal_opened = true;
+                    imageName = 'ts';
+                    title = 'TypeScript';
+                    description = 'WEB系開発アルバイト、このポートフォリオサイト等で一部使用。調べながらであればある程度扱える。';
+                "></li>
+                <li><img src="/assets/logo/sql.png" @click="
+                    is_modal_opened = true;
+                    imageName = 'sql';
+                    title = 'SQL系';
+                    description = '学校のデータベースでの授業やWEB系開発アルバイトで使用。主に顧客や商品のデータに関する一連の処理などを実装することが出来る。必要なクエリについては都度調べながら実装できる。';
+                "></li>
+                <li><img src="/assets/logo/php.png" @click="
+                    is_modal_opened = true;
+                    imageName = 'php';
+                    title = 'PHP';
+                    description = '中学校時の携帯ゲーム「Minecraft:PE」でのサーバープラグイン開発で主に使用。そのため従来のPHPの使い方の理解とは少し異なるが、基礎的な文法などの知識はある。当時は1万行クラスのコードを書いており、チーム戦のゲームなども作っていた。';
+                "></li>
             </ul>
         </div>
         <div class="skill-row">
-            <img class="star" src="/assets/logo/star1.png">
+            <img class="star" src="/assets/logo/star1.png" @click="
+                is_modal_opened = true;
+                imageName = 'star1';
+                title = '技術レベル1';
+                description = '授業などで触れたことがある程度。実用的なプログラムの開発経験はない';
+            ">
             <div class="split-line"></div>
             <ul class="skill">
-                <li><img src="/assets/logo/docker.png"></li>
-                <li><img src="/assets/logo/ros.png"></li>
+                <li><img src="/assets/logo/docker.png" @click="
+                    is_modal_opened = true;
+                    imageName = 'docker';
+                    title = 'Docker';
+                    description = '開発過程で時々扱う程度で、あまり理解度は高くない。一部のコマンドの理解と、コンテナの仕組みについての基礎知識がある程度。';
+                "></li>
+                <li><img src="/assets/logo/ros.png" @click="
+                    is_modal_opened = true;
+                    imageName = 'ros';
+                    title = 'ROS';
+                    description = '大学でのNHKロボコンへ向けてのシミュレーター制作で使用。Gazebo、rvizなどと併せてWSL上で開発を行った。正直調べながらなんとなくで実装しており、理解しているかは微妙。';
+                "></li>
             </ul>
         </div>  
         <div class="text">
