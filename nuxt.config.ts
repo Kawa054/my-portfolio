44a0317b247e2a5d3f652ff3e53014af60c66ed1
kawa054 <kawa054ss@gmail.com>
2025-09-07 05:18:13 +0900

    detail update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,5 +8,8 @@ export default defineNuxtConfig({
 				{ id:"MathJax-script", src: 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js', async: true}
 			]
 		}  
-	}
+	},
+	build: {
+    	transpile: ['three']
+  	}
 })
